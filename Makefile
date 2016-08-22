114d8287459f7453f54cb9529d959c75559f9979
King's Way <io@stdio.io>
2016-08-22 23:06:06 +0800

    make test encrypt & decrypt

--- Makefile
+++ Makefile
@@ -11,8 +11,10 @@ main.o: main.c
 des_cfb64.o: des_cfb64.c des_cfb64.h
 	cc $(cflags) -c des_cfb64.c
 
-test:
-	echo -n "type=2&provice=32&school=南京信息工程大学&name=张祯杰&examroom=&m=77-18-7A-F5-4F-D5" | iconv -t gbk | xargs ./main
+test:main
+	tmp=`echo -n "type=2&provice=32&school=南京信息工程大学&name=张三&examroom=&m=77-88-99-11-22-33" | iconv -t gbk | xargs ./main encrypt` ;\
+	echo ;\
+	echo $$tmp | xargs ./main decrypt | iconv -f gbk
 
 .PHONY : clean
 clean :
--- main.c
+++ main.c
@@ -6,25 +6,24 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
+	if (argc < 3)
 		return 0;
-	//int fd = open("input.txt", O_RDONLY);	
-	//char msg[100] = {};
-	//read(fd, msg, 100);
-	
-	//char *key = "> buvOQ^";
 	char *key = "QghdW;O;";
+	char *result = NULL;
 
-	char *result = Encrypt(key,argv[1],strlen(argv[1]));
+	if(strcmp("encrypt",argv[1])==0)
+		result = Encrypt(key,argv[2],strlen(argv[2]));
+	else if(strcmp("decrypt",argv[1])==0)
+		result = Decrypt(key,argv[2],strlen(argv[2]));
 	
 	int j=0;
 	for(j=0;j<strlen(result);j++)
 		printf("%c", result[j]);
 	
-	printf("\n\n");
-	
-	for(j=0;j<strlen(result);j++)
-		printf("%02x", result[j]&0xff);
+	// 
+	//printf("\n\n");
+	//for(j=0;j<strlen(result);j++)
+	//	printf("%02x", result[j]&0xff);
 	
 	return 0;
 }
