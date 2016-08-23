aca08a51be5d2ac4a46c4f5ae4db88535574ec75
King's Way <io@stdio.io>
2016-08-23 18:37:02 +0800

    encrypt and decrypt in main

--- main.c
+++ main.c
@@ -8,13 +8,14 @@ int main(int argc, char **argv)
 {
 	if (argc < 3)
 		return 0;
-	char *key = "QghdW;O;";
+	char *encrypt_key = "QghdW;O;";
+	char *decrypt_key = "121yO7d=";
 	char *result = NULL;
 
 	if(strcmp("encrypt",argv[1])==0)
-		result = Encrypt(key,argv[2],strlen(argv[2]));
+		result = Encrypt(encrypt_key,argv[2],strlen(argv[2]));
 	else if(strcmp("decrypt",argv[1])==0)
-		result = Decrypt(key,argv[2],strlen(argv[2]));
+		result = Decrypt(decrypt_key,argv[2],strlen(argv[2]));
 	
 	int j=0;
 	for(j=0;j<strlen(result);j++)
